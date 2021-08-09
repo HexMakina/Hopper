14e4e903ad6561d60e1e316003b5b819e810d7f5
sam <sammy@hexmakina.be>
2021-08-09 23:24:12 +0200

    Hopper initial commit for package release

---
+++ RouterException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HexMakina\Hopper;
+
+class RouterException extends \Exception
+{
+  public function __construct($message, $code = 0, $previous = null)
+  {
+    parent::__construct('KADRO_ROUTER_ERR_'.$message, $code, $previous);
+  }
+}
