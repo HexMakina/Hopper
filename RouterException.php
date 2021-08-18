19d7cc9d23d6b9548190b7029875e9b5caf681ed
sam <sammy@hexmakina.be>
2021-08-18 23:45:34 +0200

    PSR-12, phpcbf autofix

--- RouterException.php
+++ RouterException.php
@@ -4,8 +4,8 @@ namespace HexMakina\Hopper;
 
 class RouterException extends \Exception
 {
-  public function __construct($message, $code = 0, $previous = null)
-  {
-    parent::__construct('KADRO_ROUTER_ERR_'.$message, $code, $previous);
-  }
+    public function __construct($message, $code = 0, $previous = null)
+    {
+        parent::__construct('KADRO_ROUTER_ERR_' . $message, $code, $previous);
+    }
 }
