535c71815539ac629febd54949f5be4cf47cb43a
sam <sammy@hexmakina.be>
2021-09-02 22:25:47 +0200

    cleanup: order and commented out functions

--- RouterInterface.php
+++ RouterInterface.php
@@ -8,37 +8,30 @@ interface RouterInterface
     const REQUEST_POST = 'POST';
     const ROUTE_HOME_NAME = 'home';
 
+    public function mapHomeRoute($route);
     public function routeExists($route): bool;
 
     public function match($requestUrl = null, $requestMethod = null);
+    public function targetController();
+    public function targetMethod();
+    public function name();
 
-  // generates URL for href and location
-    public function hyp($route, $route_params = []);
-
-  // heads to another location
-    public function hop($route = null, $route_params = []);
-
-  // do you GET it ?
+    // do you GET it ?
     public function requests(): bool;
 
-  // have you POSTed it ?
+    // have you POSTed it ?
     public function submits(): bool;
+
+    // generates URL for href and location
+    public function hyp($route, $route_params = []);
+
+    // heads to another location
+    public function hop($route = null, $route_params = []);
     public function submitted($param_name = null);
 
     public function sendFile($file_path);
 
-
     public function webRoot(): string;
-    // public function file_root(): string;
-
-    public function mapHomeRoute($route);
-
     public function filePath($setter = null): string;
     public function basePath($setter = null): string;
-
-    // public function set_file_root($setter);
-
-    public function targetController();
-    public function targetMethod();
-    public function name();
 }
